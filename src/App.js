9576ee525dd63f23d4aab075b784948354edd03b
jtdevsource <joseph@jtdevs.com>
2019-07-19 15:03:37 -0500

    visual game board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import GameBoard from './components/GameBoard';
 
 function App() {
+
+  const boardSize = {numRows: 6, numCols: 7};
+
   return (
     <div className="App">
-      
+      <GameBoard boardSize={boardSize}/>
     </div>
   );
 }
