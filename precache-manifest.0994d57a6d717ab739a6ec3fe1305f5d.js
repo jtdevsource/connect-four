133bb978bad658b6558ec04ef69841fd17c01c2a
jtdevsource <joseph@jtdevs.com>
2019-07-22 15:26:58 -0500

    Updates

---
+++ precache-manifest.0994d57a6d717ab739a6ec3fe1305f5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1cf29ef5d1165ae2f4d5f44a0b1929",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "ab2b7e3233fa2ec73407",
+    "url": "/connect-four/static/css/main.83272b11.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/connect-four/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "ab2b7e3233fa2ec73407",
+    "url": "/connect-four/static/js/main.97e34c91.chunk.js"
+  },
+  {
+    "revision": "5f78906a733342f2d383",
+    "url": "/connect-four/static/js/runtime~main.000e00dd.js"
+  }
+]);
